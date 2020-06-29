58ed6d49e9b9f28b72c8415a8ec7833550f93c81
Jan Grosser <email@jan-grosser.de>
2020-06-29 20:09:18 +0200

    Added feature to switch between keyboard sequences

--- push2talk.ino
+++ push2talk.ino
@@ -6,7 +6,7 @@
  * key(s) are pressed/released connected to specific pins of microcontroller.
  * 
  * VERSION:
- * v0.1
+ * v0.2
  * 
  * AUTHOR:
  * (c) Jan Grosser, email@jan-grosser.de
@@ -41,6 +41,11 @@
 int p2t_pin = 6;      // push-to-talk (p2t) button
 int conf_pin = 7;     // config (conf) button
 
+// Create and initialize a variable holding the selected keyboard sequnce.
+// Later, this variable is used to specify the sequence to use from a list
+// of predefined sequnces.
+int sequence = 0;
+
 // Create bounce objects to debounce buttons. Debouncing time t_debounce is
 // in milliseconds
 int t_debounce = 10;
@@ -68,16 +73,64 @@ void setup() {
   pinMode(conf_pin, INPUT_PULLUP);
 }
 
-void send_ctrl_m() {
-  Keyboard.set_modifier(MODIFIERKEY_CTRL);
-  Keyboard.send_now();
-  Keyboard.set_key1(KEY_M);
-  Keyboard.send_now();
-  Keyboard.set_modifier(0);
-  Keyboard.set_key1(0);
-  Keyboard.send_now();
+//
+int sequence_switcher(int sequence, char mode, char edge) {
+  int seq_total = 2; // total number of sequnces defined
+
+  // mode == p (permutate)
+  if (mode == 'p') {
+    sequence = sequence + 1;
+    sequence = sequence % seq_total;
+    switch (sequence) {
+      case 0:
+        Keyboard.println("Using Sequence CTRL+m CTRL+m");
+        break;
+      case 1:
+        Keyboard.println("Using Sequence WindowsKey+F4 WindowsKey+F4");
+        break;
+    }
+  }
+  // mode == e (execute)
+  if (mode == 'e') {
+    switch (sequence) {
+      case 0:
+        send_ctrl_m__ctrl_m(edge);
+        break;
+      case 1:
+        send_win_f4__win_f4(edge);
+        break;
+    }
+  }
+  return sequence;
 }
 
+// Many programs for web conferencing using shortcut CTRL+M for both
+// mute and unmute
+void send_ctrl_m__ctrl_m(char edge) {
+  // for both falling and rising edge send the same keyboard sequence
+  if ((edge == 'f') || (edge == 'r')) {
+    Keyboard.set_modifier(MODIFIERKEY_CTRL);
+    Keyboard.send_now();
+    Keyboard.set_key1(KEY_M);
+    Keyboard.send_now();
+    Keyboard.set_modifier(0);
+    Keyboard.set_key1(0);
+    Keyboard.send_now();
+  }
+}
+
+// Skye for Business uses WindowsKey+F4, both for mute and unmute
+void send_win_f4__win_f4(char edge) {
+  if ((edge == 'f' || edge == 'r')) {
+    Keyboard.set_modifier(MODIFIERKEY_GUI); // WindowsKey
+    Keyboard.send_now();
+    Keyboard.set_key1(KEY_F4);
+    Keyboard.send_now();
+    Keyboard.set_modifier(0);
+    Keyboard.set_key1(0);
+    Keyboard.send_now();
+  }
+}
 
 void loop() {
   // Update all buttons
@@ -86,19 +139,22 @@ void loop() {
 
   // Check for falling edges (key pressed events)
   if (p2t_button.fallingEdge()) {
-    // Keyboard.println("p2t button pressed");
-    send_ctrl_m();
+    //send_win_f4__win_f4('f');
+    //send_ctrl_m__ctrl_m('f');
+    sequence = sequence_switcher(sequence, 'e', 'f');
   }
   if (conf_button.fallingEdge()) {
-    // Keyboard.println("conf button pressed");
+    // Do nothing here, change configuration only if we sense a rising
+    // edge of the config button.
   }
 
   // Check for rising edges (key release event)
   if (p2t_button.risingEdge()) {
-    // Keyboard.println("p2t button released");
-    send_ctrl_m();
+    //send_win_f4__win_f4('r');
+    //send_ctrl_m__ctrl_m('r');
+    sequence = sequence_switcher(sequence, 'e', 'r');
   }
   if (conf_button.risingEdge()) {
-    // Keyboard.println("conf button released");
+    sequence = sequence_switcher(sequence, 'p', 'r');
   }
 }
